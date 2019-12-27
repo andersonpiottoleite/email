c8ca75b55ea418218e547091bd40c26fb422297e
anderson <anderson.leite@gpnet.com.br>
2019-12-27 16:17:50 -0300

    empurrando o projeto

--- email/src/main/java/com/email/email/EmailApplication.java
+++ email/src/main/java/com/email/email/EmailApplication.java
@@ -20,8 +20,8 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 @SpringBootApplication
 public class EmailApplication implements CommandLineRunner {
 
-	 @Autowired
-	    private JavaMailSender javaMailSender;
+	@Autowired
+	private JavaMailSender javaMailSender;
 	 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailApplication.class, args);
@@ -29,20 +29,17 @@ public class EmailApplication implements CommandLineRunner {
 	
 	@Override
     public void run(String... args) throws MessagingException, IOException {
-
         System.out.println("Sending Email...");
 
         //sendEmail();
 		sendEmailWithAttachment();
 
         System.out.println("Done");
-
     }
     // em caso de problemas http://www.codersdesks.com/javax-mail-authenticationfailedexception-535-5-7-8-username-and-password-not-accepted/
 	// lista de emails: //http://listadeemailmarketinggratis.blogspot.com/2013/10/lista-1-6857-e-mails.html
 		
-    void sendEmail() {
-
+    private void sendEmail() {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo("piottok10@gmail.com");
 
@@ -50,18 +47,16 @@ public class EmailApplication implements CommandLineRunner {
         msg.setText("Hello World \n Spring Boot Email");
 
         javaMailSender.send(msg);
-
     }
     
-    void sendEmailWithAttachment() throws MessagingException, IOException {
-    	
+    private void sendEmailWithAttachment() throws MessagingException, IOException {
     	File arquivoEmails  = new File("C:\\Users\\anderson.leite\\Desktop\\emails2.txt");
     	
     	Scanner scanner  = new Scanner(arquivoEmails);
     	
     	StringBuilder emails = new StringBuilder();
     	
-    	int qtdeLimitEmails = 1000; 
+    	int qtdeLimitEmails = 50; 
     	
     	while (scanner.hasNext()) {
     		emails.append(scanner.nextLine());
@@ -81,11 +76,9 @@ public class EmailApplication implements CommandLineRunner {
     	envia(emails);
     	
     	scanner.close();
-
     }
 
 	private void envia(StringBuilder emails) throws MessagingException, AddressException {
-
         MimeMessage msg = javaMailSender.createMimeMessage();
 
         // true = multipart message
@@ -103,48 +96,47 @@ public class EmailApplication implements CommandLineRunner {
         //helper.setText("Check attachment for image!");
 
         // true = text/html
-        helper.setText(
-        			"<html>"
-        		
-	        			+ "<body>"
-        					
-			        		+ "<h2 style='color:black font-family: 'Verdana', Times, serif;'>Olá!</h2>"
-			        		
-			        		+ "<h1 style='color:black font-family: 'Georgia', Times, serif;'>Somos a <b style='color:#CD6600'><i>Casa do Evangelho</i></b>, uma família que pela graça, vive e prega o verdadeiro Evangelho de <b style='color:#CD6600'><i>Jesus Cristo</i></b>!</h1>"
-			        		
-			        		+ "</br>"
-			        		
-			        		+ "<h3 style='color:black font-family: 'Lucida Console', Times, serif;'>Queremos te convidar para asssitir uma de nossas reflexões:</h3>"
-			        		
-			        		+ "</br>"
-			        		
-			        		+ "<a style='color:#CD6600' href=\"https://www.youtube.com/watch?v=avyiSZs4-Lw&feature=youtu.be\"><h3><b style='color:#CD3333;'>YouTube</b> - A glória da segunda casa!</h3></a>"
-			        		
-							+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>De seu <b style='color:#CD6600'><i>LIKE</i></b>, se <b style='color:#CD6600'><i>increva</i></b> em nosso Canal e <b style='color:#CD6600'><i>compartilhe!</i></b></h4>"
-							
-							+ "</br>"
-							
-							+ "</br>"
-							
-							+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>Deus te abençoe!</h4>"
-
-			        		+ "<img src='cid:LogoCasaDoEvangelho' style='float:left;width:200px;height:200px;'/>"
-			        		
-		        		+ "</body>"
-		        		
-	        		+"</html>"
-		        		
-	        		, true);
+        helper.setText(getConteudoEmail(), true);
         
         //enviar anexo
         //FileSystemResource foto = new FileSystemResource("C:\\Users\\anderson.leite\\Desktop\\LogoCasaDoEvangelho.jpg");
         // helper.addAttachment("LogoCasaDoEvangelho.jpg", foto);
         
-        helper.addInline("LogoCasaDoEvangelho",
-                new File("C:\\Users\\anderson.leite\\Desktop\\Imagens\\Logo andersona casa do en.jpg"));
-        
+        helper.addInline("LogoCasaDoEvangelho", new File("C:\\Users\\anderson.leite\\Desktop\\Imagens\\Logo andersona casa do en.jpg"));
         
         javaMailSender.send(msg);
+	}
+
+	private String getConteudoEmail() {
+		return "<html>"
+    		
+			+ "<body>"
+				
+				+ "<h2 style='color:black font-family: 'Verdana', Times, serif;'>Olá!</h2>"
+				
+				+ "<h1 style='color:black font-family: 'Georgia', Times, serif;'>Somos a <b style='color:#CD6600'><i>Casa do Evangelho</i></b>, uma família que pela graça, vive e prega o verdadeiro Evangelho de <b style='color:#CD6600'><i>Jesus Cristo</i></b>!</h1>"
+				
+				+ "</br>"
+				
+				+ "<h3 style='color:black font-family: 'Lucida Console', Times, serif;'>Queremos te convidar para assitir uma de nossas reflexões:</h3>"
+				
+				+ "</br>"
+				
+				+ "<a style='color:#CD6600' href=\"https://www.youtube.com/watch?v=avyiSZs4-Lw&feature=youtu.be\"><h3><b style='color:#CD3333;'>YouTube</b> - A glória da segunda casa!</h3></a>"
+				
+				+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>De seu <b style='color:#CD6600'><i>LIKE</i></b>, se <b style='color:#CD6600'><i>increva</i></b> em nosso Canal e <b style='color:#CD6600'><i>compartilhe!</i></b></h4>"
+				
+				+ "</br>"
+				
+				+ "</br>"
+				
+				+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>Deus te abençoe!</h4>"
+
+				+ "<img src='cid:LogoCasaDoEvangelho' style='float:left;width:200px;height:200px;'/>"
+				
+			+ "</body>"
+			
+		+"</html>";
 	}
 
 }
