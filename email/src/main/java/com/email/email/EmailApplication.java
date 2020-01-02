1ae10fa8f7c1953aa141930bc51e84a18997dde6
anderson <anderson.leite@gpnet.com.br>
2020-01-02 11:23:47 -0300

    empurrando o projeto

--- email/src/main/java/com/email/email/EmailApplication.java
+++ email/src/main/java/com/email/email/EmailApplication.java
@@ -50,7 +50,7 @@ public class EmailApplication implements CommandLineRunner {
     }
     
     private void sendEmailWithAttachment() throws MessagingException, IOException {
-    	File arquivoEmails  = new File("C:\\Users\\anderson.leite\\Desktop\\emails2.txt");
+    	File arquivoEmails  = new File("C:\\Users\\anderson.leite\\Desktop\\emails1.txt");
     	
     	Scanner scanner  = new Scanner(arquivoEmails);
     	
@@ -66,7 +66,7 @@ public class EmailApplication implements CommandLineRunner {
 			}
 			
 			if(emails.length() > qtdeLimitEmails) {
-				
+				 System.out.println("Enviando emails...");
 				envia(emails);
 				
 				emails.setLength(0);
@@ -90,13 +90,15 @@ public class EmailApplication implements CommandLineRunner {
         // esconde os de mais destinarários
         helper.setBcc(InternetAddress.parse(emails.toString()));
 
-        helper.setSubject("Casa do Evangelho | Reflexão | A glória da segunda casa!");
+        //helper.setSubject("Casa do Evangelho | Reflexão | A glória da segunda casa!");
+        
+        helper.setSubject("Casa do Evangelho | Reflexão | Deus pode me perdoar?");
 
         // default = text/plain
         //helper.setText("Check attachment for image!");
 
         // true = text/html
-        helper.setText(getConteudoEmail(), true);
+        helper.setText(getConteudoEmail2(), true);
         
         //enviar anexo
         //FileSystemResource foto = new FileSystemResource("C:\\Users\\anderson.leite\\Desktop\\LogoCasaDoEvangelho.jpg");
@@ -107,14 +109,16 @@ public class EmailApplication implements CommandLineRunner {
         javaMailSender.send(msg);
 	}
 
-	private String getConteudoEmail() {
+	private String getConteudoEmail1() {
 		return "<html>"
     		
 			+ "<body>"
 				
-				+ "<h2 style='color:black font-family: 'Verdana', Times, serif;'>Olá!</h2>"
+				+ "<h2 style='color:black font-family: 'Verdana', Times, serif;'>Olá! </h2>"
 				
-				+ "<h1 style='color:black font-family: 'Georgia', Times, serif;'>Somos a <b style='color:#CD6600'><i>Casa do Evangelho</i></b>, uma família que pela graça, vive e prega o verdadeiro Evangelho de <b style='color:#CD6600'><i>Jesus Cristo</i></b>!</h1>"
+				+ "<h2 style='color:black font-family: 'Verdana', Times, serif;'>Pedimos sua licença para nos apresentarmos <b style='color:#CD6600'>:)</b></h2>"
+				
+				+ "<h1 style='color:black font-family: 'Georgia', Times, serif;'>Somos a <b style='color:#CD6600'><i>Casa do Evangelho</i></b>, uma família que pela graça de Deus, vive e prega o verdadeiro Evangelho de <b style='color:#CD6600'><i>Jesus Cristo</i></b>!</h1>"
 				
 				+ "</br>"
 				
@@ -122,9 +126,43 @@ public class EmailApplication implements CommandLineRunner {
 				
 				+ "</br>"
 				
-				+ "<a style='color:#CD6600' href=\"https://www.youtube.com/watch?v=avyiSZs4-Lw&feature=youtu.be\"><h3><b style='color:#CD3333;'>YouTube</b> - A glória da segunda casa!</h3></a>"
+				+ "<a style='color:#CD6600; text-decoration: underline;' href=\"https://www.youtube.com/watch?v=avyiSZs4-Lw&feature=youtu.be\"><h3><b style='color:#CD3333;'>YouTube</b> - A glória da segunda casa!</h3></a>"
 				
-				+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>De seu <b style='color:#CD6600'><i>LIKE</i></b>, se <b style='color:#CD6600'><i>increva</i></b> em nosso Canal e <b style='color:#CD6600'><i>compartilhe!</i></b></h4>"
+				+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>Se gostar, de seu <b style='color:#CD6600'><i>LIKE</i></b>, se <b style='color:#CD6600'><i>increva</i></b> em nosso Canal, <b style='color:#CD6600'><i>clique no sininho</i></b> para receber notificações de novos videos e <b style='color:#CD6600'><i>compartilhe!</i></b></h4>"
+				
+				+ "</br>"
+				
+				+ "</br>"
+				
+				+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>Deus te abençoe!</h4>"
+
+				+ "<img src='cid:LogoCasaDoEvangelho' style='float:left;width:200px;height:200px;'/>"
+				
+			+ "</body>"
+			
+		+"</html>";
+	}
+	
+	private String getConteudoEmail2() {
+		return "<html>"
+    		
+			+ "<body>"
+				
+				+ "<h2 style='color:black font-family: 'Verdana', Times, serif;'>Olá! </h2>"
+				
+				+ "<h2 style='color:black font-family: 'Verdana', Times, serif;'>Pedimos sua licença para nos apresentarmos <b style='color:#CD6600'>:)</b></h2>"
+				
+				+ "<h1 style='color:black font-family: 'Georgia', Times, serif;'>Somos a <b style='color:#CD6600'><i>Casa do Evangelho</i></b>, uma família que pela graça de Deus, vive e prega o verdadeiro Evangelho de <b style='color:#CD6600'><i>Jesus Cristo</i></b>!</h1>"
+				
+				+ "</br>"
+				
+				+ "<h3 style='color:black font-family: 'Lucida Console', Times, serif;'>Queremos te convidar para assitir uma de nossas reflexões:</h3>"
+				
+				+ "</br>"
+				
+				+ "<a style='color:#CD6600; text-decoration: underline;' href=\"https://www.youtube.com/watch?v=CbrB2yuBimM\"><h3><b style='color:#CD3333;'>YouTube</b> - Deus pode me perdoar?</h3></a>"
+				
+				+ "<h4 style='color:black font-family: 'Lucida Console', Times, serif;'>Se gostar, de seu <b style='color:#CD6600'><i>LIKE</i></b>, se <b style='color:#CD6600'><i>increva</i></b> em nosso Canal, <b style='color:#CD6600'><i>clique no sininho</i></b> para receber notificações de novos videos e <b style='color:#CD6600'><i>compartilhe!</i></b></h4>"
 				
 				+ "</br>"
 				
